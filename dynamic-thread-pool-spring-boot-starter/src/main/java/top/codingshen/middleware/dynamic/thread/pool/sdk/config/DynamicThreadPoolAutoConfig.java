8fc8056e028df8121c7239572519404cfe057098
AlexShen <73343630+Alex-Shen1121@users.noreply.github.com>
2024-08-03 22:03:05 +0800

    数据池配置数据

--- dynamic-thread-pool-spring-boot-starter/src/main/java/top/codingshen/middleware/dynamic/thread/pool/sdk/config/DynamicThreadPoolAutoConfig.java
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/top/codingshen/middleware/dynamic/thread/pool/sdk/config/DynamicThreadPoolAutoConfig.java
@@ -1,12 +1,12 @@
 package top.codingshen.middleware.dynamic.thread.pool.sdk.config;
 
-import com.alibaba.fastjson2.JSON;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import top.codingshen.middleware.dynamic.thread.pool.sdk.domain.DynamicThreadPoolService;
 
 import java.util.Map;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -22,8 +22,8 @@ public class DynamicThreadPoolAutoConfig {
 
     private final Logger logger = LoggerFactory.getLogger(DynamicThreadPoolAutoConfig.class);
 
-    @Bean("dynamicThreadPoolService")
-    public String dynamicThreadPoolService(ApplicationContext applicationContext, Map<String, ThreadPoolExecutor> threadPoolExecutorMap) {
+    @Bean("dynamicThreadPollService")
+    public DynamicThreadPoolService dynamicThreadPollService(ApplicationContext applicationContext, Map<String, ThreadPoolExecutor> threadPoolExecutorMap) {
 
         String applicationName = applicationContext.getEnvironment().getProperty("spring.application.name");
 
@@ -32,9 +32,7 @@ public class DynamicThreadPoolAutoConfig {
             logger.warn("动态线程池, 启动提示, SpringBoot 应用未配置 spring.application.name 无法获取到应用名称");
         }
 
-        logger.info("线程池信息: {}", JSON.toJSONString(threadPoolExecutorMap.keySet()));
-
-        return new String();
+        return new DynamicThreadPoolService(applicationName, threadPoolExecutorMap);
     }
 
 }
