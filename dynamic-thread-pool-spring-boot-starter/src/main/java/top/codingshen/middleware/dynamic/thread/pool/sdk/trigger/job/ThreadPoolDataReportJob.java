8045581c69c0d0022954f413bcb5df58cdbd3b93
AlexShen <73343630+Alex-Shen1121@users.noreply.github.com>
2024-08-04 00:49:15 +0800

    redis 注册中心

---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/top/codingshen/middleware/dynamic/thread/pool/sdk/trigger/job/ThreadPoolDataReportJob.java
@@ -0,0 +1,42 @@
+package top.codingshen.middleware.dynamic.thread.pool.sdk.trigger.job;
+
+import com.alibaba.fastjson2.JSON;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import top.codingshen.middleware.dynamic.thread.pool.sdk.domain.IDynamicThreadPoolService;
+import top.codingshen.middleware.dynamic.thread.pool.sdk.domain.model.entity.ThreadPoolConfigEntity;
+import top.codingshen.middleware.dynamic.thread.pool.sdk.registry.IRegistry;
+
+import java.util.List;
+
+/**
+ * @ClassName ThreadPoolDataReportJob
+ * @Description 线程池数据上报任务
+ * @Author alex_shen
+ * @Date 2024/8/4 - 00:17
+ */
+@Slf4j
+public class ThreadPoolDataReportJob {
+
+    private final IDynamicThreadPoolService dynamicThreadPoolService;
+
+    private final IRegistry registry;
+
+    public ThreadPoolDataReportJob(IDynamicThreadPoolService dynamicThreadPoolService, IRegistry registry) {
+        this.dynamicThreadPoolService = dynamicThreadPoolService;
+        this.registry = registry;
+    }
+
+    @Scheduled(cron = "0/20 * * * * ?")
+    public void execReportThreadPoolList() {
+        List<ThreadPoolConfigEntity> threadPoolConfigEntities = dynamicThreadPoolService.queryThreadPoolList();
+        registry.reportThreadPool(threadPoolConfigEntities);
+        log.info("动态线程池, 上报线程池信息:{}", JSON.toJSONString(threadPoolConfigEntities));
+
+        for (ThreadPoolConfigEntity threadPoolConfigEntity : threadPoolConfigEntities) {
+            registry.reportThreadPoolConfigParameter(threadPoolConfigEntity);
+            log.info("动态线程池, 上报线程池配置:{}", JSON.toJSONString(threadPoolConfigEntity));
+        }
+    }
+
+}
