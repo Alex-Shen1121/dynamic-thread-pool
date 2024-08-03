5f0068e0aacf85ad711951ed6905bbb24a56281f
AlexShen <73343630+Alex-Shen1121@users.noreply.github.com>
2024-08-03 17:47:30 +0800

    init project

---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/top/codingshen/middleware/dynamic/thread/pool/sdk/config/DynamicThreadPoolAutoConfig.java
@@ -0,0 +1,12 @@
+package top.codingshen.middleware.dynamic.thread.pool.sdk.config;
+
+/**
+ * @ClassName DynamicThreadPoolAutoConfig
+ * @Description 动态配置入口
+ * @Author alex_shen
+ * @Date 2024/8/3 - 17:44
+ */
+public class DynamicThreadPoolAutoConfig {
+
+
+}
---
+++ dynamic-thread-pool-spring-boot-starter/src/test/java/top/codingshen/middleware/dynamic/thread/pool/ApiTest.java
@@ -0,0 +1,10 @@
+package top.codingshen.middleware.dynamic.thread.pool;
+
+/**
+ * @ClassName ApiTest
+ * @Description 单元测试
+ * @Author alex_shen
+ * @Date 2024/8/3 - 17:46
+ */
+public class ApiTest {
+}
