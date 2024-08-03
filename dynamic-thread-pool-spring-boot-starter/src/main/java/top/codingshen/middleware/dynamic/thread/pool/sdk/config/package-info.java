9df2617ad92c8215ac154cd8334e19dd5aefafa7
AlexShen <73343630+Alex-Shen1121@users.noreply.github.com>
2024-08-03 20:52:05 +0800

    创建基础 springboot 工程

---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/top/codingshen/middleware/dynamic/thread/pool/sdk/config/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @ClassName package-info
+ * @Description auto config 配置入口
+ * @Author alex_shen
+ * @Date 2024/8/3 - 17:42
+ */
+package top.codingshen.middleware.dynamic.thread.pool.sdk.config;
